05205dbf5f79196aaac65807db454596e1d00edd
Conrad Koh <conradkoh@gmail.com>
2020-02-29 00:37:45 +0800

    Added new textfield behavior without mandatory field

--- components/input/TextField.js
+++ components/input/TextField.js
@@ -1,29 +1,57 @@
+import * as React from 'react';
 import MUITextField from '@material-ui/core/TextField';
 /**
- * @param {TextFieldProps} props 
+ * @extends {React.Component<TextFieldProps>}
  */
-export default function TextField(props) {
-    let { label, type, 
-        value, errorText = '', 
-        autocomplete, multiline, rows } = props;
-    let error = errorText ? true : false;
-    let default_styles = getDefaultStyles();
-    return (<MUITextField
-        style={props.style ? {...default_styles, ...props.style } : default_styles}
-        InputLabelProps={{ shrink: ([null, undefined, ''].indexOf(value) > 0) ? false : true }}
-        value={(value == undefined || value == null) ? '' : value}
-        error={error}
-        label={label}
-        onChange={formatOnChangeParams(props.onChange)}
-        type={type}
-        helperText={errorText}
-        autoComplete={autocomplete}
-        fullWidth={true}
-        multiline={multiline}
-        rows={rows}
-    />)
+export default class TextField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+    }
+    isPropsValueUsed() {
+        let keys = Object.keys(this.props);
+        if(keys.includes('value')) {
+            return true;
+        }
+        return false;
+    }
+    async onChange(e) {
+        let onChange = this.props.onChange || (() => {});
+        let value = e.target.value;
+        await new Promise((resolve, reject) => {
+            this.setState({ value }, () => resolve());
+        });
+        await onChange(value);
+    }
+    render() {
+        let props = this.props;
+        let { label, type,
+            value: propValue, errorText = '',
+            autocomplete, multiline, rows } = props;
+        let { value: stateValue } = this.state;
+        let value = this.isPropsValueUsed() ? propValue : stateValue;
+        let error = errorText ? true : false;
+        let default_styles = getDefaultStyles();
+        
+        return (<MUITextField
+            style={props.style ? {...default_styles, ...props.style } : default_styles}
+            InputLabelProps={{ shrink: ([null, undefined, ''].indexOf(value) > 0) ? false : true }}
+            value={(value == undefined || value == null) ? '' : value}
+            error={error}
+            label={label}
+            onChange={this.onChange.bind(this)}
+            type={type}
+            helperText={errorText}
+            autoComplete={autocomplete}
+            fullWidth={true}
+            multiline={multiline}
+            rows={rows}
+        />)
+    }
 }
-function getDefaultStyles () {
+function getDefaultStyles() {
     /**
      * @type {React.CSSProperties}
      */
@@ -31,13 +59,6 @@ function getDefaultStyles () {
         marginTop: '1em'
     };
     return style;
-}
-
-function formatOnChangeParams(onChangeNew) {
-    let fn = onChangeNew || ((v) => { });
-    return ((event) => {
-        fn(event.target.value);
-    });
 }
 /**
  * @typedef TextFieldProps
@@ -52,6 +73,6 @@ function formatOnChangeParams(onChangeNew) {
  * @property {React.CSSProperties} [style]
  */
 
- /**
-  * @typedef {'username' | 'password' | 'given-name' | 'family-name'} HTMLAutocompleteTypes
-  */
+/**
+ * @typedef {'username' | 'password' | 'given-name' | 'family-name'} HTMLAutocompleteTypes
+ */
