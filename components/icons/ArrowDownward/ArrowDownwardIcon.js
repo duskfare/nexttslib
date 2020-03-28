b8e2d11cce33a752c37db3e186f191c1bb61773e
Conrad Koh <conradkoh@gmail.com>
2020-03-28 23:15:56 +0800

    Added upward and downard icons

---
+++ components/icons/ArrowDownward/ArrowDownwardIcon.js
@@ -0,0 +1,3 @@
+import MUIArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+export const ArrowDownwardIcon = MUIArrowDownwardIcon;
+export default ArrowDownwardIcon;
---
+++ components/icons/ArrowUpward/ArrowUpwardIcon.js
@@ -0,0 +1,3 @@
+import MUIArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+export const ArrowUpwardIcon = MUIArrowUpwardIcon;
+export default ArrowUpwardIcon;
