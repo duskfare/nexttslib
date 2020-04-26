e4f9ee51a4f8da4ee533cb099462b04388ac703d
Conrad Koh <conradkoh@gmail.com>
2020-04-26 16:04:51 +0800

    Refactored dialog props defintions as typedef

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -6,22 +6,29 @@ import MUIDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
-import DialogActionsButton from '../../components/dialog/Dialog'
+import DialogActionsButton from '../../components/dialog/Dialog';
 import { Button, CloseButton } from '../button/index';
 import { externalClasses } from '../../theme/override';
 
-const DialogTitle = props => {
+const DialogTitle = (props) => {
     const { children, classes, onClose, ...other } = props;
-    return (<div style={{ display: 'flex' }}>
-        <MUIDialogTitle disableTypography {...other} style={{ display: 'flex', flexDirection: 'row', width: '100%', padding: '16px 24px 0px 24px' }}>
-            <div style={{ display: 'flex', flex: 1, alignItems: 'center' }}>
-                <Typography variant="h6" style={{ flex: '1' }}>{children}</Typography>
-            </div>
-            <CloseButton onClick={onClose.bind(this)}/>
-        </MUIDialogTitle>
-    </div>
+    return (
+        <div style={{ display: 'flex' }}>
+            <MUIDialogTitle
+                disableTypography
+                {...other}
+                style={{ display: 'flex', flexDirection: 'row', width: '100%', padding: '16px 24px 0px 24px' }}
+            >
+                <div style={{ display: 'flex', flex: 1, alignItems: 'center' }}>
+                    <Typography variant="h6" style={{ flex: '1' }}>
+                        {children}
+                    </Typography>
+                </div>
+                <CloseButton onClick={onClose.bind(this)} />
+            </MUIDialogTitle>
+        </div>
     );
-}
+};
 
 /** @type {Object<string, React.CSSProperties>} */
 const styles = {
@@ -32,29 +39,34 @@ const styles = {
     },
 };
 /**
- * 
- * @param {{open: boolean}} props 
+ *
+ * @param {{open: boolean}} props
  */
-let DialogContainer = (props => (
+let DialogContainer = (props) => (
     <MUIDialog {...props} classes={{ paper: `${props.classes.dialogPaper} ${externalClasses.mui.dialogPaper}` }}></MUIDialog>
-));
+);
 DialogContainer = withStyles(styles)(DialogContainer);
 /**
- * @extends {React.Component<{open: boolean, title: string, onClose: function},{}>}
+ * @extends {React.Component<DialogProps>}
  */
 class Dialog extends React.Component {
     render() {
-        let { title = '', onClose = (() => {}) } = this.props;
+        let { title = '', onClose = () => {} } = this.props;
         return (
-            <DialogContainer open={this.props.open || false} >
+            <DialogContainer open={this.props.open || false}>
                 <DialogTitle id="customized-dialog-title" onClose={onClose}>
                     {title}
                 </DialogTitle>
-                <div style={{ flexGrow: 1, padding: '0px 30px 0px 30px', overflow: 'auto' }}>
-                    {this.props.children}
-                </div>
+                <div style={{ flexGrow: 1, padding: '0px 30px 0px 30px', overflow: 'auto' }}>{this.props.children}</div>
             </DialogContainer>
         );
     }
 }
 export default Dialog;
+
+/**
+ * @typedef DialogProps
+ * @property {boolean} open
+ * @property {string} title
+ * @property {function} onClose
+ */
