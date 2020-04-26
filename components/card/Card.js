c83f3c1fdc26b69bb579b37bf01ca02669bffb5f
Conrad Koh <conradkoh@gmail.com>
2020-04-26 16:04:13 +0800

    Fixed card showing error messages for style rendering

--- components/card/Card.js
+++ components/card/Card.js
@@ -3,28 +3,16 @@ import MUICardHeader from '@material-ui/core/CardHeader';
 import MUICardActions from '@material-ui/core/CardActions';
 import { makeStyles } from '@material-ui/core/styles';
 import * as React from 'react';
-let useStyles = makeStyles((theme) => ({
-    card: {},
-    cardHeader: {},
-    cardTitle: {
-        fontSize: '1.4em',
-    },
-    cardSubTitle: {},
-}));
 /**
  * @param {CardProps} props
  */
 export default function Card(props) {
-    let classes = useStyles(props);
+    // let classes = useStyles(props);
     return (
-        <MUICard
-            className={classes.card + ' ' + props.className}
-            style={props.style || {}}
-            variant={props.variant || 'outlined'}
-        >
-            <div className={classes.cardHeader}>
-                {props.title && <div className={classes.cardTitle}>{props.title}</div>}
-                {props.subTitle && <div className={classes.cardSubTitle}>{props.subTitle}</div>}
+        <MUICard className={props.className || ''} style={props.style || {}} variant={props.variant || 'outlined'}>
+            <div>
+                {props.title && <div style={{ fontSize: '1.4em' }}>{props.title}</div>}
+                {props.subTitle && <div>{props.subTitle}</div>}
             </div>
             {props.children}
             {props.cardActions && <MUICardActions>{props.cardActions}</MUICardActions>}
