0c908523e271ca15c46184ba04d599f7606d4f3d
Conrad Koh <conradkoh@gmail.com>
2020-06-14 18:00:34 +0800

    Added snackbar HOC

---
+++ hoc/withSnackBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '../button/IconButton';
+import CloseIcon from '../icons/Close/CloseIcon';
+import Alert from '@material-ui/lab/Alert';
+export function withSnackbar(Component) {
+    return (props) => {
+        const [open, setOpen] = useState(false);
+        /** @type { {type: NotificationSeverity, message: string }} */
+        let notificationInitialState = { type: 'info', message: '' };
+        const [notification, setNotification] = useState(notificationInitialState);
+        const handleClose = () => {
+            setOpen(false);
+        };
+        return (
+            <React.Fragment>
+                <Component
+                    {...props}
+                    notify={(type, message) => {
+                        setNotification({ type, message });
+                        setOpen(true);
+                    }}
+                />
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity={notification.type}>
+                        {notification.message}
+                    </Alert>
+                </Snackbar>
+            </React.Fragment>
+        );
+    };
+}
+
+/**
+ * @typedef {'error' | 'success' | 'warning' | 'info'} NotificationSeverity
+ */
+/**
+ * @typedef SnackBarProps
+ * @property {function(NotificationSeverity, string): *} notify
+ */
